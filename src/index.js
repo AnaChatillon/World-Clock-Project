a2292d08a81609a1d6304d2639039abcccac3f94
Ana Chatillon <a_chantil@hotmail.com>
2025-05-26 23:24:54 +0100

    Added current location and insert selected option on top

--- src/index.js
+++ src/index.js
@@ -28,26 +28,31 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
 
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML += ` 
+  citiesElement.insertAdjacentHTML(
+    "afterbegin",
+    ` 
     <div class="city">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
-    </div>`;
+      "A"
+    )}</small></div>
+    </div>`
+  );
 }
 
 updateTime();
 setInterval(updateTime, 1000);
 
-let citiesSelect = document.querySelector("$#city");
+let citiesSelectElement = document.querySelector("#city");
 
 citiesSelectElement.addEventListener("change", updateCity);
