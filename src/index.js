7cc95e1a08d8380f3c526838acde209089f685e8
Ana Chatillon <a_chantil@hotmail.com>
2025-05-26 21:08:48 +0100

    Updated JS

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//Los Angeles
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment().tz("America/Los_Angeles");
+
+losAngelesDateElement.innerHTML = losAngelesDate.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  "h:mm:ss [<small<]A[>/small]"
+);
